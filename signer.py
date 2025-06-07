d487cf005506b49aced88def53e34183d392142a
varfozel <136359642+varfozel@users.noreply.github.com>
2025-06-07 09:19:35 +0300

    Update signer.py

--- signer.py
+++ signer.py
@@ -17,9 +17,3 @@ def get_signature_from_node(private_key):
 def main(private_key):
     sig_data = get_signature_from_node(private_key)
     return sig_data['signature'], sig_data['address'], sig_data['timestamp']
-
-if __name__ == "__main__":
-    # For testing purposes
-    test_key = "0x883efd2c292029f553d86ed1d6950685ab131788dee59ecb293a8305cc80d993"
-    signature, address, timestamp = main(test_key)
-    print(signature, address, timestamp)
